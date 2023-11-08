a0c1bdba05488bb2176e7b1c5343d30725c750c5
4rad95 <148912953+4rad95@users.noreply.github.com>
2023-11-08 12:02:59 +0100

    Add files via upload

--- HW5/scripts/script.js
+++ HW5/scripts/script.js
@@ -9,26 +9,26 @@ console.log("Вторая часть домашнего задания")
 
 inputNum = prompt ("Введите 1-e число :")
 let inputNum1 = prompt ("Введите 2-e число :")
-if (inputNum > inputNum1) ( 
-        console.log("Меньшее число :"+inputNum1))
-    else ( 
-        console.log("Меньшее число :"+inputNum))
-
+if (inputNum > inputNum1) { 
+        console.log(`Меньшее число : ${inputNum1}`)}
+    else if (inputNum1>inputNum) { 
+        console.log(`Меньшее число : ${inputNum}`)}
+            else {`Они равны: ${inputNum}` }
 console.log("-------------------------------------")
 
 console.log("Третья часть домашнего задания")
 inputNum = prompt ("Введите число :")
-if (inputNum < 100) (
-        console.log("Наше число меньше 100 ")) 
+if (inputNum < 100) {
+        console.log("Наше число меньше 100 ")} 
     else if (inputNum > 100) 
-            (console.log("Наше число больше 100 ")) 
-                else (console.log("Наше число равно 100 "))
+            {console.log("Наше число больше 100 ")} 
+                else {console.log("Наше число равно 100 ")}
 
 console.log("-------------------------------------")
 
 console.log("Четвертая часть домашнего задания")
 inputNum = prompt ("Как Вас зовут? ")
 inputNum1 = prompt ("Сколько тебе лет "+ inputNum)
-if (inputNum1 < 18) (console.log ("Hi, "+inputNum))
-    else (console.log ("Hello, "+inputNum))
+if (inputNum1 < 18) {console.log ("Hi, "+inputNum)}
+    else {console.log ("Hello, "+inputNum)}
 
